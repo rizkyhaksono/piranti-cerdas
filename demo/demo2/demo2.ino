003809379529b8efd0b05b71aaf36c4bec6aa7db
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-10-22 21:16:22 +0700

    feat(demo): modul 2

--- demo/demo2/demo2.ino
+++ demo/demo2/demo2.ino
@@ -1,9 +1,10 @@
 #define LIGHT_SENSOR_PIN 36
 
 const int buzzerPin = 25;
-int LDR_Val = 0; 
 int switchState = 0;
 int ledPin= 21;   
+
+int LDR_Val = 0; 
 
 void setup() {
   Serial.begin(115200);
@@ -16,28 +17,25 @@ void loop() {
 
   LDR_Val = analogRead(LIGHT_SENSOR_PIN);
 
-  //KEGIATAN 1
+  // KEGIATAN 1
+  // if (LDR_Val < 500 ) {
+  //   digitalWrite(ledPin,LOW);
+  //   Serial.println("Output : Dark");
+  // } else if(LDR_Val > 500 && LDR_Val < 1000){
+  //   digitalWrite(ledPin, HIGH); 
+  //   delay(1000); 
+  //   digitalWrite(ledPin, LOW); 
+  //   delay(1000); 
+  //   Serial.println("Output : Redup");
+  // } else if (LDR_Val >= 1000) {
+  //   digitalWrite(ledPin, HIGH); 
+  //   Serial.println("Output : Terang");
+  // }
 
+  //KEGIATAN 2
   if (LDR_Val == LOW) {
-    digitalWrite(buzzerPin, LOW);
-  } else {
     digitalWrite(buzzerPin, HIGH);
-  }
-
-  // KEGIATAN 2
-
-
-  if (LDR_Val < 500 ) {
-    digitalWrite(ledPin,LOW);
-    Serial.println("Output : Dark");
-  } else if(LDR_Val > 500 && LDR_Val < 1000){
-    digitalWrite(ledPin, HIGH); 
-    delay(1000); 
-    digitalWrite(ledPin, LOW); 
-    delay(1000); 
-    Serial.println("Output : Redup");
-  } else if (LDR_Val >= 1000) {
-    digitalWrite(ledPin, HIGH); 
-    Serial.println("Output : Terang");
+  } else {
+    digitalWrite(buzzerPin, LOW);
   }
 }
